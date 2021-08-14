893b9b40fbd26c0a8a9c1f8cd62b0c57426e5edd
Biel <bielp07@gmail.com>
2021-08-14 12:45:20 -0300

    Start IlludGUI class.

--- illud.py
+++ illud.py
@@ -26,3 +26,21 @@ class Buffer(object):
         line = self.lines[row1][:col1] + text + self.lines[row2][col2:]
         self.lines[row1:row2+1] = line.split('\n')
 
+class IlludGUI(object):
+    def __init__(self, stdscr, filename):
+        self.screen = stdscr
+        
+        text = ''
+        
+        if(filename != None and os.path.isfile(filename)):
+            with open(filename) as f:
+                text = f.read()
+        self.fileName = filename
+        self.buf = Buffer(text)
+        self.row = 0
+        self.col = 0
+        self.scrollTop = 0
+        self.mode = 'Normal'
+        self.message = 'Illud'
+        self.exitEditor = False
+
