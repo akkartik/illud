0b31c59ac6a611d35c7b5234ff76d47a4d16a5f0
Biel <bielp07@gmail.com>
2021-08-14 12:41:20 -0300

    Start Buffer class.

--- illud.py
+++ illud.py
@@ -2,3 +2,19 @@ import curses
 import os
 from sys import argv
 from contextlib import contextmanager
+
+class Buffer(object):
+    def __init__(self, text = ''):
+        self.lines = text.split('\n')
+
+    def getLines(self):
+        return list(self.lines)
+
+    def checkPoint(self, row, col):
+        if(row < 0 or row > len(self.lines) - 1):
+            print("Row exceeded the limit: '{}'".format(row))
+
+        currentRow = self.lines[row]
+        
+        if(col < 0 or col > len(currentRow)):
+            print("Column exceeded the limit: '{}'".format(col))
