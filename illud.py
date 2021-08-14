f85e28602e0eb5d515cc26262071bb96fd8c6444
Biel <bielp07@gmail.com>
2021-08-14 12:42:36 -0300

    Finish Buffer class.

--- illud.py
+++ illud.py
@@ -18,3 +18,11 @@ class Buffer(object):
         
         if(col < 0 or col > len(currentRow)):
             print("Column exceeded the limit: '{}'".format(col))
+
+    def setText(self, row1, col1, row2, col2, text):
+        self.checkPoint(row1, col1)
+        self.checkPoint(row2, col2)
+
+        line = self.lines[row1][:col1] + text + self.lines[row2][col2:]
+        self.lines[row1:row2+1] = line.split('\n')
+
