9a4df01d746f5c546c05c287d5444af5e685cd70
Biel <bielp07@gmail.com>
2021-08-15 09:41:15 -0300

    Remove unused function.

--- src/illud.py
+++ src/illud.py
@@ -10,19 +10,7 @@ class Buffer(object):
     def getLines(self):
         return list(self.lines)
 
-    def checkPoint(self, row, col):
-        #if(row < 0 or row > len(self.lines) - 1):
-        #    print("Row exceeded the limit: '{}'".format(row))
-
-        currentRow = self.lines[row]
-        
-        #if(col < 0 or col > len(currentRow)):
-        #   print("Column exceeded the limit: '{}'".format(col))
-
     def setText(self, row1, col1, row2, col2, text):
-        self.checkPoint(row1, col1)
-        self.checkPoint(row2, col2)
-
         line = self.lines[row1][:col1] + text + self.lines[row2][col2:]
         self.lines[row1:row2+1] = line.split('\n')
 
